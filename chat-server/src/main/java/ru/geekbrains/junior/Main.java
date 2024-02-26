41ebf89b54748a2b057b607fe4c745d50b170393
Li <varlamova_e.t@mail.ru>
2024-02-26 10:07:37 +0300

    test

--- chat-server/src/main/java/ru/geekbrains/junior/Main.java
+++ chat-server/src/main/java/ru/geekbrains/junior/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(3500);
         Server server = new Server(serverSocket);
+        System.out.println("mur");
     }
 }
